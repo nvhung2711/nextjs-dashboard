1c823e84c8589efaf4ea3bde785df0894179e5e8
nvhung2711 <nvhung2711@gmail.com>
2026-01-06 14:37:10 +1100

    form validation for creating invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -9,26 +9,45 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        message: 'Please select a customer'
+    }),
+    amount: z.coerce.number().gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+        message: 'Please select an invoice status'
+    }),
     date: z.string(),
 });
 
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+}
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+export async function createInvoice(prevState: State, formData: FormData) {
+    const validatedFields = CreateInvoice.safeParse(Object.fromEntries(formData.entries()));
 
-export async function createInvoice(formData: FormData) {
-    const rawFormData = Object.fromEntries(formData.entries());
-    const { customerId, amount, status } = CreateInvoice.parse(rawFormData);
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.'
+        }
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
     try {
         await sql`
-            INSERT INTO invoices (customer_id, amount, status, date)
-            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
         `;
     } catch (error) {
         console.error(error);
@@ -40,15 +59,17 @@ export async function createInvoice(formData: FormData) {
     redirect('/dashboard/invoices');
 }
 
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function updateInvoice(id: string, formData: FormData) {
     const { customerId, amount, status } = UpdateInvoice.parse(Object.fromEntries(formData.entries()));
 
     const amountInCents = amount * 100;
     try {
         await sql`
-            UPDATE invoices
-            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-            WHERE id = ${id}
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
         `;
     } catch (error) {
         console.error(error);
